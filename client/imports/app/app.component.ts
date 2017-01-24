3257e7d0bbea56418139a063a3d3856b255b6ac7
Uchenna Chilaka <uche.chilaka@gmail.com>
2017-01-24 07:42:12 -0500

    Stopping to go to work

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,13 +1,33 @@
-import { Component } from "@angular/core";
-import template from "./app.component.html";
-import style from "./app.component.scss";
+import { Component } from '@angular/core';
+
+import template from './app.component.html';
 
 @Component({
-  selector: "app",
-  template,
-  styles: [ style ]
+    selector: 'app',
+    template
 })
+
 export class AppComponent {
-  constructor() {
-  }
+    // declares parties property of component as array of type Any
+    parties: any[];
+
+    constructor() {
+        this.parties = [
+            {
+                'name': 'Dubstep-Free Zone',
+                'description': 'Can we please just for an evening not listen to dubstep.',
+                'location': 'Palo Alto'
+            },
+            {
+                'name': 'All dubstep all the time',
+                'description': 'Get it on!',
+                'location': 'Palo Alto'
+            },
+            {
+                'name': 'Savage lounging',
+                'description': 'Leisure suit required. And only fiercest manners.',
+                'location': 'San Francisco'
+            }
+        ];
+    }
 }
--- client/main.ts
+++ client/main.ts
@@ -1,11 +1,9 @@
-import "angular2-meteor-polyfills";
+// create main entrypoint for app
+import 'angular2-meteor-polyfills';
 
-import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
-import { enableProdMode } from "@angular/core";
-import { AppModule } from "./imports/app";
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-enableProdMode();
+import { AppModule } from './imports/app/app.module';
 
-Meteor.startup(() => {
-   platformBrowserDynamic().bootstrapModule(AppModule);
-});
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
